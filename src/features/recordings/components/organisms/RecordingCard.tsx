541339b68d941e41d57f05ffabac58263ef8db39
Dave <davidglogowski2003@gmail.com>
2025-08-30 23:30:37 +0200

    Extract Recording card components into seperate organisms

--- src/features/recordings/components/organisms/RecordingCard.tsx
+++ src/features/recordings/components/organisms/RecordingCard.tsx
@@ -1,4 +1,5 @@
-import { HeroIcon } from "../../../../components/atoms/HeroIcon";
+import { RecordingImageContainer } from "./RecordingImageContainer";
+import { RecordingTextContainer } from "./RecordingTextContainer";
 
 type RecordingOrganismProps = {
   title: string;
@@ -18,28 +19,13 @@ export function RecordingCard({
   return (
     <div className="w-full h-full">
       <div className="w-full flex gap-4 h-full">
-        <div className="p-2 border w-80 min-w-80 max-w-80 flex flex-col justify-between">
-          <img className="object-cover w-full h-40" src={image} />
-          <div className="w-full flex justify-between items-center">
-            <div className="flex gap-1 py-1 items-center">
-              <HeroIcon iconName="EyeIcon" />
-              <p>2</p>
-            </div>
-            <div className="flex gap-1 py-1 items-center">
-              <HeroIcon iconName="ClockIcon" />
-              <p>130m</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h2 className="text-3xl font-bold text-leidenBlue">{title}</h2>
-          <p>Owner: {owner}</p>
-          <div className="flex gap-2 text-leidenBlue font-bold">
-            <HeroIcon solid={true} iconName="FolderIcon" />
-            <p>{course}</p>
-          </div>
-          <p>{description}</p>
-        </div>
+        <RecordingImageContainer image={image} />
+        <RecordingTextContainer
+          title={title}
+          owner={owner}
+          course={course}
+          description={description}
+        />
       </div>
     </div>
   );
---
+++ src/features/recordings/components/organisms/RecordingTextContainer.tsx
@@ -0,0 +1,26 @@
+import { HeroIcon } from "../../../../components/atoms/HeroIcon";
+
+type RecordingTextContainerProps = {
+  title: string;
+  owner: string;
+  course: string;
+  description: string;
+};
+export function RecordingTextContainer({
+  title,
+  owner,
+  course,
+  description,
+}: RecordingTextContainerProps) {
+  return (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-3xl font-bold text-leidenBlue">{title}</h2>
+          <p>Owner: {owner}</p>
+          <div className="flex gap-2 text-leidenBlue font-bold">
+            <HeroIcon solid={true} iconName="FolderIcon" />
+            <p>{course}</p>
+          </div>
+          <p>{description}</p>
+        </div>
+  );
+}
