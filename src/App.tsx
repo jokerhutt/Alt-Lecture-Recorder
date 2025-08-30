61625f894d76f27779908acb9b477026be12850d
Dave <davidglogowski2003@gmail.com>
2025-08-30 23:50:05 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/organisms/Header/Header";
-import { HomeGrid } from "./features/home/components/organisms/HomeGrid";
 import { MainNavigation } from "./components/organisms/Navigation/MainNavigation";
 import { registerModals } from "./modals/modalRegistry";
 import { PageTemplate } from "./layouts/PageTemplate";
 
 function App() {
-  const [count, setCount] = useState(0);
   registerModals();
 
   return (
-    <div className="h-dvh w-dvw flex flex-col">
-      <header className="w-full">
-        <Header />
-      </header>
+    <Router>
+      <div className="h-dvh w-dvw flex flex-col">
+        <header className="w-full">
+          <Header />
+        </header>
 
-      <div className="w-full h-full flex">
-        <div className="h-full w-1/4">
-          <div className="hidden h-full lg:block">
-            <MainNavigation />
+        <div className="w-full h-full flex">
+          <div className="h-full w-1/4">
+            <div className="hidden h-full lg:block">
+              <MainNavigation />
+            </div>
+          </div>
+          <div className="w-full">
+            <Routes>
+              <Route path="/" element={<PageTemplate />} />
+              <Route path="/recordings" element={<PageTemplate />} />
+            </Routes>
           </div>
         </div>
-        <div className="w-full">
-          <PageTemplate />
-        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
