a16cdda0d0510fa4ba48cb740d5d8c298edcda29
Dave <davidglogowski2003@gmail.com>
2025-08-30 15:50:07 +0200

    Add dashboard cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './Header/Header'
+import { HomeGrid } from './Card/HomeGrid'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
-    <>
-    <header>
+    <div className='h-dvh w-dvw flex flex-col'>
+    <header className='w-full'>
         <Header/>
     </header>
 
-    </>
+    <div>
+      <HomeGrid/>
+    </div>
+
+    </div>
   )
 }
 
