dad1c9415fcb0fa2e5c43748e7750f5257c879b9
Dave <davidglogowski2003@gmail.com>
2025-08-31 18:20:13 +0200

    Add navigation for home button in header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -2,8 +2,12 @@ import { openEditDrawer } from "../../../modals/modalRegistry";
 import { HeroIcon } from "../../atoms/HeroIcon";
 import { Searchbar } from "../../molecules/SearchBar/Searchbar";
 import { Avatar } from "../../atoms/Avatar";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+
+  const navigate = useNavigate()
+
   return (
     <div className="bg-leidenBlue px-4 py-2 flex justify-between items-center w-full h-20">
       <div
@@ -12,11 +16,11 @@ export function Header() {
       >
         <HeroIcon iconName="Bars3Icon" />
       </div>
-      <div className="text-white lg:flex hidden items-center w-full gap-2 font-bold">
+      <div onClick={() => navigate("/")} className="text-white lg:flex hidden hover:cursor-pointer items-center gap-2 font-bold">
         <HeroIcon className="h-10 w-10" iconName="HomeIcon" />
         <p className="text-xl">Home</p>
       </div>
-      <div className="w-full flex gap-4 items-center justify-end h-full">
+      <div className="w-1/2 flex gap-4 items-center justify-end h-full">
         <Avatar userName="DG" />
         <Searchbar />
       </div>
